05472684c51cb483f13772269f49b67ad50af915
luiz4a <159453767+luiz4a@users.noreply.github.com>
2024-08-05 09:09:00 -0300

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ifelse;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Ifelse {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int maior;
+        
+        
+        if (a > b) {
+            maior = a;
+        } else { 
+            maior = b;
+        }
+        
+        System.out.println("O maior numero é: " + maior);
+    }
+}
